14982731ab5f676b9c142a0784f8bd6cd5e6dbb5
m3lt1ngmetals-dot <m3lt1ngmetals@gmail.com>
2026-04-03 21:34:53 -0500

    Create eventHandler.js

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+  const eventFiles = fs.readdirSync('./src/events')
+    .filter(file => file.endsWith('.js'));
+
+  for (const file of eventFiles) {
+    const event = require(`../events/${file}`);
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+};
