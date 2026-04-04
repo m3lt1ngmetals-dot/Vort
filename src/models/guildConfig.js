374b4be7e2280c9ebbe3d615845992c22b0d89c3
m3lt1ngmetals-dot <m3lt1ngmetals@gmail.com>
2026-04-03 22:12:34 -0500

    Update guildConfig.js

--- src/models/guildConfig.js
+++ src/models/guildConfig.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  guildId: String,
-  antiSpam: Object,
-  antiNuke: Object,
-  whitelist: Object,
-  logChannelId: String
+const guildConfigSchema = new mongoose.Schema({
+  guildId: { type: String, required: true },
+
+  antiSpam: {
+    enabled: { type: Boolean, default: true },
+    messageLimit: { type: Number, default: 5 },
+    interval: { type: Number, default: 3000 }
+  },
+
+  antiNuke: {
+    enabled: { type: Boolean, default: true },
+    channelDeleteLimit: { type: Number, default: 3 },
+    roleDeleteLimit: { type: Number, default: 3 },
+    timeWindow: { type: Number, default: 10000 }
+  },
+
+  whitelist: {
+    users: { type: [String], default: [] },
+    roles: { type: [String], default: [] }
+  },
+
+  logChannelId: { type: String }
 });
 
-module.exports = mongoose.model("GuildConfig", schema);
-whitelist: {
-  users: { type: [String], default: [] },
-  roles: { type: [String], default: [] }
-}
+module.exports = mongoose.model('GuildConfig', guildConfigSchema);
