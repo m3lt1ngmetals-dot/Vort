6b8bebf9800853f2485e7ded606c101db325a0ee
m3lt1ngmetals-dot <m3lt1ngmetals@gmail.com>
2026-04-04 13:15:28 -0500

    Update eventHandler.js

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -5,7 +5,26 @@ module.exports = (client) => {
     .filter(file => file.endsWith('.js'));
 
   for (const file of eventFiles) {
-    const event = require(`../events/${file}`);
-    client.on(event.name, (...args) => event.execute(...args, client));
+    try {
+      const event = require(`../events/${file}`);
+
+      if (!event?.name || typeof event.execute !== "function") {
+        console.warn(`[EVENT SKIP] Invalid event file: ${file}`);
+        continue;
+      }
+
+      client.on(event.name, (...args) => {
+        try {
+          event.execute(...args, client);
+        } catch (err) {
+          console.error(`[EVENT ERROR] ${file}`, err);
+        }
+      });
+
+      console.log(`[EVENT LOADED] ${event.name}`);
+
+    } catch (err) {
+      console.error(`[EVENT LOAD FAILED] ${file}`, err);
+    }
   }
 };
