cd167d58f1cefb84d064219441f7a066862d1e6c
m3lt1ngmetals-dot <m3lt1ngmetals@gmail.com>
2026-04-03 21:32:47 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const client = new Client({
+  intents: Object.values(GatewayIntentBits),
+  partials: Object.values(Partials)
+});
+
+client.commands = new Collection();
+client.prefix = "?";
+
+// MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Handlers
+require('./handlers/commandHandler')(client);
+require('./handlers/eventHandler')(client);
+
+client.login(process.env.TOKEN);
